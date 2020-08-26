be90902b9c67ff66fa20b68f4f1b080ef472bbd6
rbtkay <rbtkay@pop-os.localdomain>
2020-08-26 11:52:46 +0200

    toast if no product were found with scan

---
+++ src/components/Toast.jsx
@@ -0,0 +1,16 @@
+import { ToastAndroid } from "react-native";
+const Toast = ({ visible, message }) => {
+    if (visible) {
+        ToastAndroid.showWithGravityAndOffset(
+            message,
+            ToastAndroid.LONG,
+            ToastAndroid.BOTTOM,
+            25,
+            50
+        );
+        return null;
+    }
+    return null;
+};
+
+export default Toast;
--- src/screens/ScannerScreen.jsx
+++ src/screens/ScannerScreen.jsx
@@ -5,6 +5,7 @@ import { Camera } from "expo-camera";
 const { FlashMode: CameraFlashModes, Type: CameraTypes } = Camera.Constants;
 
 import styles from "../../assets/styles/style";
+import Toast from "../components/Toast";
 
 const ScannerScreen = ({ navigation }) => {
     const [isFlashOn, toggleFlash] = useState(false);
@@ -14,6 +15,7 @@ const ScannerScreen = ({ navigation }) => {
     const [cameraPermission, setCameraPermission] = useState(null);
     const [cameraType, setCameraType] = useState(Camera.Constants.Type.back);
     const [scanned, setScanned] = useState(null);
+    const [isToastVisible, setVisibleToast] = useState(false);
 
     const changeFlash = () => {
         isFlashOn ? toggleFlash(false) : toggleFlash(true);
@@ -32,12 +34,17 @@ const ScannerScreen = ({ navigation }) => {
                     navigation.navigate("Details", {
                         product_id: data,
                     });
+                } else { 
+                    setVisibleToast(true);
                 }
             })
             .catch((error) => {
                 console.error(error);
             });
     };
+
+    //detect the change on visible toast and reset it to false
+    useEffect(() => setVisibleToast(false), [isToastVisible]);
 
     useEffect(() => {
         Camera.requestPermissionsAsync().then(({ status }) => {
@@ -67,6 +74,7 @@ const ScannerScreen = ({ navigation }) => {
                 onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
                 style={styles.camera}
             >
+                <Toast visible={isToastVisible} message="No products found!" />
                 <Button title={"Flash"} onPress={() => changeFlash()} />
                 {/*                 
                     <Button title={'Recommencer'} onPress={()=> this.setState({scanned: null})} /> */}
